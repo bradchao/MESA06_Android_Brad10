2e0096024aea26236cadbaa0a27132c2f82d334d
Brad Chao <brad@brad.tw>
2016-11-21 09:24:28 +0800

    v4

--- app/src/main/java/com/example/user/brad10/MainActivity.java
+++ app/src/main/java/com/example/user/brad10/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.user.brad10;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences sp;
@@ -36,5 +41,23 @@ public class MainActivity extends AppCompatActivity {
         String name = sp.getString("user","nobody");
         tv.setText("Stage: " + stage + "\n" +
                 "User: " + name);
+    }
+
+    // 內存 write
+    public void test3(View v){
+        // 在 內存空間之下的 FileSystem
+        try {
+            FileOutputStream fout = openFileOutput("brad.data", MODE_PRIVATE);
+            fout.write("Hello, Brad".getBytes());
+            fout.flush();
+            fout.close();
+        } catch (IOException e) {
+            Log.v("brad", e.toString());
+        }
+
+    }
+    // 內存 read
+    public void test4(View v){
+
     }
 }
